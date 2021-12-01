13b5b1986c0867ff7e3f5c596f212b89e9f118a0
Praj78 <86727228+Praj78@users.noreply.github.com>
2021-12-01 17:05:04 +0530

    Update area.js

--- area.js
+++ area.js
@@ -4,16 +4,18 @@ const output=document.querySelector("#output")
 areabtn.addEventListener("click",calculatearea)
 
 function calculateproduct(a,b,c){
-    if(a>0 && b>0 && c>0){
+    if(a<0 || b<0 ||c<0){
+output.innerText="invalid input ,enter the valid input"
+    }else{
 const s =(a+b+c)/2
 const sum =s*(s-a)*(s-b)*(s-c)
 return sum
-    }
+    } 
 }
 
 function calculatearea(){
     const product= calculateproduct(Number(sides[0].value),Number(sides[1].value),Number(sides[2].value))
     const sum =Math.sqrt(product)
-    output.innerText="the area of triangle is: "+sum+" units"
+    output.innerText="the area of triangle is: "+sum.toFixed(2)+" units"
 
 }
