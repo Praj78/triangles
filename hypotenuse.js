b6dd45de11a2878f30f4fdb5108d4834e350fd5d
Praj78 <86727228+Praj78@users.noreply.github.com>
2021-12-01 17:08:13 +0530

    Update hypotenuse.js

--- hypotenuse.js
+++ hypotenuse.js
@@ -4,10 +4,12 @@ const output=document.querySelector("#output")
 function calculateHypotenuse(){
 const sumOfSquares=calculateSumOfSquares(Number(sides[0].value),Number(sides[1].value))
 const lengthOfHypotenuse=Math.sqrt(sumOfSquares)
-output.innerText="the length of hypotenuse is: "+lengthOfHypotenuse
+output.innerText="the length of hypotenuse is: "+lengthOfHypotenuse.toFixed(2)
 }
 function calculateSumOfSquares(a,b){
-    if(a>0 && b>0){
+    if(a<0 || b<0){
+output.innerText="invalid input ,enter the valid input"
+    }else{
     const sumOfSquares=a*a+b*b;
     return sumOfSquares
     }
